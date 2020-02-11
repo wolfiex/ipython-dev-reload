c2cd7531eafc54521d4ef25204945f1edd2cefb3
Daniel Ellis <daniel.ellis@york.ac.uk>
2020-02-11 13:48:48 +0000

    conda upload

---
+++ condaupdate.sh
@@ -0,0 +1,9 @@
+#sudo conda install conda-build
+#conda install anaconda-client
+#anaconda login
+
+conda-build .
+
+conda convert --platform all /opt/anaconda3/conda-bld/osx-64/ipyreload-1.2-py37_0.tar.bz2 -o converted/
+
+anaconda upload ./converted/*/*.tar.bz2
