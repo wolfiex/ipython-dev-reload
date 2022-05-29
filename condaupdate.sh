d659e158a77d52836d036e45408e6c69c9a2d95c
Dan Ellis <daniel.ellis.research@googlemail.com>
2022-05-29 17:07:57 +0100

    v1.3

--- condaupdate.sh
+++ condaupdate.sh
@@ -2,8 +2,11 @@
 #conda install anaconda-client
 #anaconda login
 
+rm -rf ~/opt/*conda3/conda-bld/osx-64/ipyreload-*.tar.bz2 ;
+rm -rf ./converted/*/ ;
+
 conda-build .
 
 conda convert --platform all ~/opt/*conda3/conda-bld/osx-64/ipyreload-*.tar.bz2 -o converted/
 
-anaconda upload ./converted/*/*.tar.bz2
+anaconda upload ./converted/*/*.tar.bz2 --force
